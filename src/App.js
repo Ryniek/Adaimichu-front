749eaec6711e2ac7365fe16274350c0f15c04b57
Michał Ryński <michalrynski96@gmail.com>
2022-11-19 21:10:12 +0100

    Imported MUI, bootstrap, roboto font, mui icons and created basic login form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import './App.css';
+import "./App.css";
+import Login from './components/login/Login';
 
 function App() {
   return (
-    <div className="container">
-  <h1>Hello</h1>
-  <p>Test</p>
-  <li>
-          <button
-            onClick={event => alert(event.target.id)}
-          >
-            <span role="img" aria-label="grinning face" id="grinning face">😀</span>
-          </button>
-        </li>
-  </div>
+    <div>
+      <Login></Login>
+    </div>
   );
 }
 
